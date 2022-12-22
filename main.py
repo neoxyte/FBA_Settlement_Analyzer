3ac3e8cf73586a57883675b049d44ad4ae550023
Michael <499674+neoxyte@users.noreply.github.com>
2022-12-22 12:54:42 -0500

    gitignore updated

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('statement.txt')
+#This stops data truncation
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+pd.set_option("display.max_colwidth", None)
+
+#open file
+flat_file = pd.read_table('statement.txt', sep='\t',dtype='unicode')
+
+print(flat_file)
