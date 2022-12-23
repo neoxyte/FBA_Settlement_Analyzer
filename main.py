fd3821a228dd86090e335f0453b5e80f4ac654f9
Michael <499674+neoxyte@users.noreply.github.com>
2022-12-23 10:31:52 -0500

    Added datatypes

--- main.py
+++ main.py
@@ -5,7 +5,40 @@ pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option("display.max_colwidth", None)
 
-#open file
-flat_file = pd.read_table('statement.txt', sep='\t',dtype='unicode')
+#data types for settlement flat file v2
+'''dtypes = {
+    "settlement-id": "category",
+    "settlement-start-date": "category",
+    "settlement-end-date": "category",
+    "deposit-date": "category",
+    "total-amount": "category",
+    "currency": "category",
+    "order-id": "category",
+    "merchant-order-id": "category",
+    "adjustment-id": "category",
+    "shipment-id": "category",
+    "marketplace-name": "category",
+    "amount-type": "category",
+    "amount-description": "category",
+    "amount": "category",
+    "fulfillment-id": "category",
+    "posted-date": "category",
+    "posted-date-time": "category",
+    "order-item-code": "category",
+    "merchant-order-item-id": "category",
+    "merchant-adjustment-item-id": "category",
+    "sku": "category",
+    "quantity-purchased": "category",
+    "promotion-id": "category",
+}'''
 
-print(flat_file)
+#open file
+df = pd.read_table('statement.txt', sep='\t', low_memory=False)
+
+
+#obtain total of fba Fees
+fba_fee_by_sku = df.loc[df['amount-description'] == 'FBAPerUnitFulfillmentFee']
+fba_fee_by_sku = pd.melt(fba_fee_by_sku , id_vars =['sku'], value_vars =['amount'])
+fba_fee_by_sku.groupby("sku")
+
+#do top three lines for each line item needed
