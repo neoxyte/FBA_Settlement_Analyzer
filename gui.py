1f401f335aae7146a6c9e47e7acebd13dbe0f1eb
Michael <499674+neoxyte@users.noreply.github.com>
2023-01-30 12:44:03 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -2,17 +2,13 @@ import PySimpleGUI as sg
 
 
 # Very basic form.  Return values as a list
-form = sg.FlexForm('Settlement Analyzer')  # begin with a blank form
-
+output_form= sg.FlexForm('Settlement Analyzer')
 layout = [
-          [sg.Text('Please select Flat File')],
-          [sg.Text('Flatfile:', size=(50, 1)), sg.FileBrowse()],
-          #[sg.Radio("Use Helium 10 cost", "Radio1", default=False)],
-          #[sg.Radio("Add Advertising Report", "Radio2", default=False)],
-          [sg.Submit(), sg.Cancel()]
-         ]
-
-
-button, filename = form.Layout(layout).Read() 
-print(button, filename)
-form.close()
+        [sg.Text('Please type a file name')],
+        [sg.Input()],
+        [sg.Submit(), sg.Cancel()]
+        ]
+button, output_name =  output_form.Layout(layout).Read() 
+#output_filename= output_name['Browse']
+print(output_name[0])
+output_form.close()
